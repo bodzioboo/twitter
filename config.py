821fc1b2ee08b9a73fc67eb88478c6c692c548d1
bodzioboo <pjbogdanski@gmail.com>
2020-04-21 19:18:02 +0200

    Uploaded all

---
+++ config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+consumer_key = "cOuJBSVOEZiaZghpEls4ykW6U"
+consumer_secret = "xCrTJaKzACr1raLjd8Z0kM2FnOn9lwPRzsG7Qrt4CyyTIVdhWQ"
+access_token = "813393750483406848-u3TeJ2GfRiFvE0YhudgGeCkxLcmo9JR"
+access_secret = "wnV70351FGyfR56yl83nN89w4WVoEZAvrt5pTbQnXd7sp"
+
